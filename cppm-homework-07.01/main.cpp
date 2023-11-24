0c84dc44d022e2875569386e87b4bfbde6e442f0
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-11-24 19:56:48 +0300

    07.01_First commit

--- cppm-homework-07.01/main.cpp
+++ cppm-homework-07.01/main.cpp
@@ -7,8 +7,60 @@
 
 #include <iostream>
 
+#define MODE 3
+
+
+class Math
+{
+private:
+    int x;
+    int y;
+public:
+    Math (int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    
+    int add (int x, int y)
+    {
+        int sum = x+y;
+        return sum;
+    }
+};
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+#ifndef MODE
+#error Please define MODE
+#endif
+    
+#if MODE == 0
+    std::cout << "Working in trainig mode =/" << std::endl;
+#endif
+    
+#if MODE == 1
+    std::cout << "Working in full mode =)" << std::endl;
+    int x; int y;
+    std::cout << "Please enter first number: ";
+    std::cin >> x;
+    std::cout << std::endl;
+    std::cout << "Please enter second number: ";
+    std::cin >> y;
+    std::cout << std::endl;
+    Math m(x,y);
+    std::cout << "Your result is: " << m.add(x,y) << std::endl;
+    
+#endif
+    
+#if MODE != 1 && MODE != 0
+    std::cout << "Unknown mode. Shutdown =(" << std::endl;
+#endif
+    
+        
+    
+   
     return 0;
 }
